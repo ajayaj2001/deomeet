cfc78d55ed669b477e72b2d0eeb71f9e06caf0ee
Ajay <65935140+ajayaj2001@users.noreply.github.com>
2021-12-21 10:43:48 +0530

    error fixed

--- src/views/Timer/index.js
+++ src/views/Timer/index.js
@@ -31,6 +31,7 @@ const Timer = () => {
     return function cleanup() {
       clearInterval(timerId);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return <div>{`${time}  | Timer : ${counter} Mins`} </div>;
